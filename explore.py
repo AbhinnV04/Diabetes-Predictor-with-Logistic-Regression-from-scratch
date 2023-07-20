765d304eb13cf9f1a04690adb646f0e12d122c44
Abhinn Verma <60428230+AbhinnV04@users.noreply.github.com>
2023-07-20 18:54:54 +0530

    Add files via upload

---
+++ explore.py
@@ -0,0 +1,70 @@
+import streamlit as st 
+import pickle 
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv('data/diabetes.csv')
+    return df
+
+df = load_data()
+
+def load_bmi():
+    conditions = [(df['BMI'] >= 25.0),
+              (df['BMI'] <= 18.5),
+              (df['BMI'] > 18.5) & (df['BMI'] < 25.0)]
+
+    choices = ['over', 'under', 'normal']
+
+    df_bmi = df[['BMI', 'Outcome']]
+    df_bmi['BMI_category'] = np.select(conditions, choices, default='normal')
+    return df_bmi
+
+def show_explore_page():
+    st.title('Exploring Diabetes Dataset')
+    st.write('Link to Dataset: https://www.kaggle.com/datasets/kandij/diabetes-dataset')
+    
+    st.image('https://files.realpython.com/media/Intro-to-Exploratory-Data-Analysis-With-Pandas_Watermarked.81a7d7df468f.jpg')
+    
+    st.write("#### A sample of the training dataset")
+    st.table(df.head(10))
+    rows, col = df.shape
+    st.write(f"""size -> {rows} x {col}""")
+    
+    st.write("#### Positive vs. Negative Outcome")
+    p_v_n = plt.figure(figsize=(6,3))
+    sns.countplot(x='Outcome', data= df)
+    st.pyplot(p_v_n)    
+    
+    st.write("#### Outcome and Age -")
+    age = plt.figure(figsize=(22,8)) 
+    sns.countplot(x='Age', hue='Outcome',data=df)
+    st.pyplot(age)
+    
+    st.write("#### Outcome and BMI -")
+    df_bmi = load_bmi()
+    bmi = plt.figure(figsize=(22,8)) 
+    sns.countplot(x='BMI_category', hue='Outcome', data=df_bmi)
+    st.pyplot(bmi)
+    
+    st.write("#### BMI with Age")
+    bmi_age = plt.figure(figsize=(12,5))
+    sns.boxplot(x='Age', y='BMI', data=df[df['Age']<61])
+    st.pyplot(bmi_age)
+    
+    
+    st.write("#### Correlation Chart")
+    corr = plt.figure(figsize=(8,6))
+    sns.heatmap(df.corr())
+    st.pyplot(corr)
+    
+    st.write("""
+             Obseravations -
+
+            - Younger People (21-30) have more negative results
+            - Overweight individuals have the highest rate of positive diagnosis
+
+             """)
---
+++ predict.py
@@ -0,0 +1,39 @@
+import streamlit as st 
+import pickle 
+import numpy as np 
+
+with open('data/model.pk1', 'rb') as file:
+    data = pickle.load(file)      
+model = data['model']
+
+def predict_page():
+    st.title("Diabetes Prediction Model")
+    
+    st.image('https://d2jx2rerrg6sh3.cloudfront.net/images/news/ImageForNews_733883_16711528404605542.jpg')
+    
+    st.write("""### We need Some information for prediction""")
+    age = st.slider('Age', 0, 100, 50)
+    pregnancies = st.slider('Number of Pregnancies', 0, 20, 6)
+    bmi = st.slider('BMI', max_value = 60., min_value= 0., value=33.6 ,step=0.10)
+    glucose = st.number_input('Glocuse level(mg/dL)', 0, 300, 148)
+    blood_pressure = st.number_input('Blood pressure', 0, 150, 72)
+    skin_thickness = st.number_input('Skin thickness', 0, 120, 35)
+    insulin = st.number_input('Insulin', 0, 850, 0)
+    pedigree_function = st.number_input('Diabetes Pedigree Function', min_value = 0.,
+                                        max_value = 2.5, step=0.01, value=0.63) 
+    st.write("""
+             *(Model type = 'Logistic Regression'*\n
+             Model accuracy = 80%*)*
+             """)
+    calculate = st.button('Calculate')
+    
+    if calculate:
+        X = np.array([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, pedigree_function, age]])
+        X = X.astype(float)
+        
+        outcome = model.predict(X)
+        if outcome == 1:
+            outcome_output = 'Positive'
+        else:
+            outcome_output = 'Negative'
+        st.subheader(f"Outcome: {outcome_output}")
